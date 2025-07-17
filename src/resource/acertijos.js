f29460b3c2800f14e450556a39568cf4fd2ac64c
Ftima08 <fatimaof99@gmail.com>
2025-07-17 21:22:56 +0200

    primera subida de código
    Solo acertijo 1,2,3

---
+++ src/resource/acertijos.js
@@ -0,0 +1,12 @@
+const acertijos = {
+  acertijo1: '2410',
+  acertijo2: 'NEGRO',
+  acertijo3: '352',
+  acertijo4: 'ESPEJO',
+  acertijo5: '846',
+  acertijo6: 'RBN',
+  acertijo7: 'CONCIENCIA',
+  acertijo8: '1',
+  acertijo9: 'LIB',
+  acertijo10: 'LIBERTAD'
+};
